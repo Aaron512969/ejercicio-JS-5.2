caa4e8a65025f30bfe411e4301bc43b0071269c7
Aaron Bolívar Robles <abr00@iesemilidarder.com>
2025-05-21 10:28:20 +0200

    Add files via upload

---
+++ ejercicio5_2.js
@@ -0,0 +1,14 @@
+function init_maxim(){
+
+    let numer1 = document.getElementById("num1").value;
+    let numer2 = document.getElementById("num2").value;
+
+    if (numer1 > numer2) {
+        document.getElementById("texto").innerHTML = numer1
+    }
+
+    else if (numer1 < numer2) {
+        document.getElementById("texto").innerHTML = numer2
+    }
+
+}
